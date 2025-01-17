67028973c3d3dfee629da92d8b84b8a387d35d33
zaluzha <hey.aku.zaky@gmail.com>
2025-01-17 12:07:37 +0700

    add login be

---
+++ apps/api/src/middlewares/auth.middleware.ts
@@ -0,0 +1,41 @@
+import { body, validationResult } from "express-validator";
+import { Request, Response, NextFunction } from "express";
+
+
+export const validateRegisterData = [
+  body("name").notEmpty().withMessage("Name is required"),
+  body("email").isEmail().withMessage("Valid email is required"),
+  body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 characters long"),
+  body("userType").isIn(["Customer", "Event Organizer"]).withMessage("User type must be either 'Participant' or 'Organizer'"),
+
+  (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({
+        status: "error",
+        message: errors.array(),
+        data: null
+      });
+      return;
+    }
+    next();
+  },
+];
+
+export const validateLoginData = [
+  body("email").isEmail().withMessage("Valid email is required"),
+  body("password").notEmpty().withMessage("Password is required"),
+
+  (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({
+        status: "error",
+        message: errors.array(),
+        data: null
+      });
+      return;
+    }
+    next();
+  },
+];
